40be376808839fb95e315f3372cb66b994fd6117
5fyt <2977366691@qq.com>
2024-08-03 09:13:52 +0800

    feat:_bem规则

---
+++ packages/hooks/use-namespace/index.ts
@@ -0,0 +1,65 @@
+import { ref } from 'vue'
+const statePrefix = 'is-'
+//规则化生成class类名
+const _bem = (namespace, block, blockSuffix, element, modifier) => {
+  let cls = `${namespace}-${block}`
+  if (blockSuffix) {
+    cls += `-${blockSuffix}`
+  }
+  if (element) {
+    cls += `__${element}`
+  }
+  if (modifier) {
+    cls += `--${modifier}`
+  }
+  return cls
+}
+const useNamespace = (block: string) => {
+  const namespace = ref('fz')
+  const b = (blockSuffix = '') =>
+    _bem(namespace.value, block, blockSuffix, '', '')
+  const e = (element?: string) => {
+    element ? _bem(namespace.value, block, '', element, '') : ''
+  }
+  const m = (modifier?: string) => {
+    modifier ? _bem(namespace.value, block, '', '', modifier) : ''
+  }
+  const be = (blockSuffix?: string, element?: string) => {
+    blockSuffix && element
+      ? _bem(namespace.value, block, blockSuffix, element, '')
+      : ''
+  }
+  const bm = (blockSuffix?: string, modifier?: string) => {
+    blockSuffix && modifier
+      ? _bem(namespace.value, block, blockSuffix, '', modifier)
+      : ''
+  }
+  const em = (element?: string, modifier?: string) => {
+    element && modifier
+      ? _bem(namespace.value, block, '', element, modifier)
+      : ''
+  }
+  const bem = (blockSuffix?: string, element?: string, modifier?: string) => {
+    blockSuffix && element && modifier
+      ? _bem(namespace.value, block, blockSuffix, element, modifier)
+      : ''
+  }
+  const is: {
+    (name: string, state: boolean | undefined): string
+    (name: string): string
+  } = (name: string, ...args: [boolean | undefined] | []) => {
+    const state = args.length >= 1 ? args[0]! : true
+    return name && state ? `${statePrefix}${name}` : ''
+  }
+  return {
+    b,
+    e,
+    m,
+    be,
+    em,
+    bm,
+    bem,
+    is,
+  }
+}
+export default useNamespace
